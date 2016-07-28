cc55ca51edd05ef9723c7f458cbfef73d5d369d5
Jiansen <jiansenhe@gmail.com>
2016-07-28 22:07:40 +0100

    verify #258 and #256: add without +x to tasks/pack.sh, e2e.sh shall fail, and CI shall fail

--- tasks/e2e.sh
+++ tasks/e2e.sh
@@ -47,7 +47,7 @@ perl -i -p0e 's/bundledDependencies.*?]/bundledDependencies": []/s' package.json
 
 # Pack react-scripts
 npm install
-scripts_path=$PWD/`npm pack`
+scripts_path=$PWD/`tasks/pack.sh`
 
 # lint
 ./node_modules/.bin/eslint --ignore-path .gitignore ./
